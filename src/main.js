8001c231d6d3794e04e2ed18da211dfe08999ac7
wk <786634240@qq.com>
2020-09-15 04:12:04 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+axios.defaults.baseURL = ' https://www.easy-mock.com/mock/5f5fc891ed072c1818dd6ae2/zufang'
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false
 
